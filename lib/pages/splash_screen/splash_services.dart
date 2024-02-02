3c862d757bf75607cb13f2602a097108187fa8fe
ARUNAV BORTHAKUR <122561140+Arunav47@users.noreply.github.com>
2024-02-02 21:41:43 +0530

    Structure change

--- lib/pages/splash_screen/splash_services.dart
+++ lib/pages/splash_screen/splash_services.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 // import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:yours/authentication/loginpage/login_page.dart';
+import 'package:yours/pages/authentication/loginpage/login_page.dart';
 
 
 class SplashServices{
